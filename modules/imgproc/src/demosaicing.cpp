cc003b1bcb2fd115fc1be656fad396f553c91e38
Shekhar <chandrashekhar.bhosale@acellere.com>
2019-10-11 16:11:16 +0530

    commitMessage : Adding some MSA specific optimizations for imgproc/video/vidoeio/dnn …

--- modules/imgproc/src/demosaicing.cpp
+++ modules/imgproc/src/demosaicing.cpp
@@ -538,6 +538,233 @@ public:
         return 0;
     }
 };
+#elif CV_MSA
+class SIMDBayerInterpolator_8u
+{
+public:
+    SIMDBayerInterpolator_8u()
+    {
+    }
+
+    int bayer2Gray(const uchar* bayer, int bayer_step, uchar* dst,
+                   int width, int bcoeff, int gcoeff, int rcoeff) const
+    {
+        /*
+         B G B G | B G B G | B G B G | B G B G
+         G R G R | G R G R | G R G R | G R G R
+         B G B G | B G B G | B G B G | B G B G
+         */
+        v8u16 masklo = msa_dupq_n_u16(255);
+        const uchar* bayer_end = bayer + width;
+
+        for( ; bayer <= bayer_end - 18; bayer += 14, dst += 14 )
+        {
+            v8u16 r0 = msa_ld1q_u16((const ushort*)bayer);
+            v8u16 r1 = msa_ld1q_u16((const ushort*)(bayer + bayer_step));
+            v8u16 r2 = msa_ld1q_u16((const ushort*)(bayer + bayer_step * 2));
+
+            v8u16 b1_ = msa_addq_u16(msa_andq_u16(r0, masklo), msa_andq_u16(r2, masklo));
+            v8u16 b1 = (v8u16)msa_extq_s16((v8i16)b1_, (v8i16)b1_, 1);
+            v8u16 b0 = msa_addq_u16(b1_, b1);
+            // b0 = b0 b2 b4 ...
+            // b1 = b1 b3 b5 ...
+
+            v8u16 g0 = msa_addq_u16(msa_shrq_n_u16(r0, 8), msa_shrq_n_u16(r2, 8));
+            v8u16 g1 = msa_andq_u16(r1, masklo);
+            g0 = msa_addq_u16(g0, msa_addq_u16(g1, (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1)));
+            v8u16 rot = (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1);
+            g1 = msa_shlq_n_u16(rot, 2);
+            // g0 = b0 b2 b4 ...
+            // g1 = b1 b3 b5 ...
+
+            r0 = msa_shrq_n_u16(r1, 8);
+            r1 = msa_addq_u16(r0, (v8u16)msa_extq_s16((v8i16)r0, (v8i16)r0, 1));
+            r0 = msa_shlq_n_u16(r0, 2);
+            // r0 = r0 r2 r4 ...
+            // r1 = r1 r3 r5 ...
+
+            b0 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(b0), (short)(rcoeff * 2)));
+            b1 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(b1), (short)(rcoeff * 4)));
+
+            g0 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(g0), (short)(gcoeff * 2)));
+            g1 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(g1), (short)(gcoeff * 2)));
+
+            r0 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(r0), (short)(bcoeff * 2)));
+            r1 = (v8u16)(msa_qdmulhq_n_s16((v8i16)(r1), (short)(bcoeff * 4)));
+
+            g0 = msa_addq_u16(msa_addq_u16(g0, b0), r0);
+            g1 = msa_addq_u16(msa_addq_u16(g1, b1), r1);
+
+            ILVRL_H2_UH(g1, g0, r0, r1);
+            msa_st1q_u8(dst, msa_rpackr_u16(r0, r1, 2));
+        }
+
+        return (int)(bayer - (bayer_end - width));
+    }
+
+    int bayer2RGB(const uchar* bayer, int bayer_step, uchar* dst, int width, int blue) const
+    {
+        /*
+         B G B G | B G B G | B G B G | B G B G
+         G R G R | G R G R | G R G R | G R G R
+         B G B G | B G B G | B G B G | B G B G
+         */
+        v8u16 masklo = msa_dupq_n_u16(255);
+        v16u8 pix[3];
+        const uchar* bayer_end = bayer + width;
+
+        for( ; bayer <= bayer_end - 18; bayer += 14, dst += 42 )
+        {
+            v8u16 r0 = msa_ld1q_u16((const ushort*)bayer);
+            v8u16 r1 = msa_ld1q_u16((const ushort*)(bayer + bayer_step));
+            v8u16 r2 = msa_ld1q_u16((const ushort*)(bayer + bayer_step * 2));
+
+            v8u16 b1 = msa_addq_u16(msa_andq_u16(r0, masklo), msa_andq_u16(r2, masklo));
+            v8u16 nextb1 = (v8u16)msa_extq_s16((v8i16)b1, (v8i16)b1, 1);
+            v8u16 b0 = msa_addq_u16(b1, nextb1);
+            // b0 b1 b2 ...
+            v8u16 zip0 = msa_rshrq_n_u16(nextb1, 1);
+            v8u16 zip1 = msa_rshrq_n_u16(b0, 2);
+            ILVRL_H2_UH(zip0, zip1, b0, b1);
+            pix[1 - blue] = msa_pack_u16(b0, b1);
+
+            v8u16 g0 = msa_addq_u16(msa_shrq_n_u16(r0, 8), msa_shrq_n_u16(r2, 8));
+            v8u16 g1 = msa_andq_u16(r1, masklo);
+            g0 = msa_addq_u16(g0, msa_addq_u16(g1, (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1)));
+            g1 = (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1);
+            // g0 g1 g2 ...
+            zip0 = g1;
+            zip1 = msa_rshrq_n_u16(g0, 2);
+            ILVRL_H2_UH(zip0, zip1, g0, g1);
+            pix[1] = msa_pack_u16(g0, g1);
+
+            r0 = msa_shrq_n_u16(r1, 8);
+            r1 = msa_addq_u16(r0, (v8u16)msa_extq_s16((v8i16)r0, (v8i16)r0, 1));
+            // r0 r1 r2 ...
+            zip0 = msa_rshrq_n_u16(r1, 1);
+            zip1 = r0;
+            ILVRL_H2_UH(zip0, zip1, r0, r1);
+            pix[1 + blue] = msa_pack_u16(r0, r1);
+
+            msa_st3q_u8(dst - 1, pix[0], pix[1], pix[2]);
+        }
+
+        return (int)(bayer - (bayer_end - width));
+    }
+
+    int bayer2RGBA(const uchar* bayer, int bayer_step, uchar* dst, int width, int blue) const
+    {
+        /*
+         B G B G | B G B G | B G B G | B G B G
+         G R G R | G R G R | G R G R | G R G R
+         B G B G | B G B G | B G B G | B G B G
+         */
+        v8u16 masklo = msa_dupq_n_u16(255);
+        v16u8 pix[4];
+        const uchar* bayer_end = bayer + width;
+        pix[3] = msa_dupq_n_u8(255);
+
+        for( ; bayer <= bayer_end - 18; bayer += 14, dst += 56 )
+        {
+            v8u16 r0 = msa_ld1q_u16((const ushort*)bayer);
+            v8u16 r1 = msa_ld1q_u16((const ushort*)(bayer + bayer_step));
+            v8u16 r2 = msa_ld1q_u16((const ushort*)(bayer + bayer_step * 2));
+
+            v8u16 b1 = msa_addq_u16(msa_andq_u16(r0, masklo), msa_andq_u16(r2, masklo));
+            v8u16 nextb1 = (v8u16)msa_extq_s16((v8i16)b1, (v8i16)b1, 1);
+            v8u16 b0 = msa_addq_u16(b1, nextb1);
+            // b0 b1 b2 ...
+            v8u16 zip0 = msa_rshrq_n_u16(nextb1, 1);
+            v8u16 zip1 = msa_rshrq_n_u16(b0, 2);
+            ILVRL_H2_UH(zip0, zip1, b0, b1);
+            pix[1 - blue] = msa_pack_u16(b0, b1);
+
+            v8u16 g0 = msa_addq_u16(msa_shrq_n_u16(r0, 8), msa_shrq_n_u16(r2, 8));
+            v8u16 g1 = msa_andq_u16(r1, masklo);
+            g0 = msa_addq_u16(g0, msa_addq_u16(g1, (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1)));
+            g1 = (v8u16)msa_extq_s16((v8i16)g1, (v8i16)g1, 1);
+            // g0 g1 g2 ...
+            zip0 = g1;
+            zip1 = msa_rshrq_n_u16(g0, 2);
+            ILVRL_H2_UH(zip0, zip1, g0, g1);
+            pix[1] = msa_pack_u16(g0, g1);
+
+            r0 = msa_shrq_n_u16(r1, 8);
+            r1 = msa_addq_u16(r0, (v8u16)msa_extq_s16((v8i16)r0, (v8i16)r0, 1));
+            // r0 r1 r2 ...
+            zip0 = msa_rshrq_n_u16(r1, 1);
+            zip1 = r0;
+            ILVRL_H2_UH(zip0, zip1, r0, r1);
+            pix[1 + blue] = msa_pack_u16(r0, r1);
+
+            msa_st4q_u8(dst - 1, pix[0], pix[1], pix[2], pix[3]);
+        }
+
+        return (int)(bayer - (bayer_end - width));
+    }
+
+    int bayer2RGB_EA(const uchar* bayer, int bayer_step, uchar* dst, int width, int blue) const
+    {
+        /*
+         B G B G | B G B G | B G B G | B G B G
+         G R G R | G R G R | G R G R | G R G R
+         B G B G | B G B G | B G B G | B G B G
+         */
+        v8u16 masklo = msa_dupq_n_u16(255);
+        v8u16 maskhi = msa_dupq_n_u16(0xFF00);
+        v16u8 pix[3];
+        const uchar* bayer_end = bayer + width;
+
+        for( ; bayer <= bayer_end - 18; bayer += 14, dst += 42 )
+        {
+            v8u16 r0 = msa_ld1q_u16((const ushort*)bayer);
+            v8u16 r1 = msa_ld1q_u16((const ushort*)(bayer + bayer_step));
+            v8u16 r2 = msa_ld1q_u16((const ushort*)(bayer + bayer_step * 2));
+
+            v8u16 b1 = msa_shrq_n_u16(msa_andq_u16(r1, maskhi), 8);
+            v8u16 nextb1 = (v8u16)msa_extq_s16((v8i16)b1, (v8i16)b1, 1);
+            v8u16 b0 = msa_addq_u16(b1, nextb1);
+            // b0 b1 b2 ...
+            v8u16 zip0 = msa_rshrq_n_u16(b0, 1);
+            v8u16 zip1 = b1;
+            ILVRL_H2_UH(zip0, zip1, b0, b1);
+            pix[2 - (blue << 1)] = msa_pack_u16(b0, b1);
+
+            v8u16 g0 = msa_andq_u16(r1, masklo);
+            v8u16 nextg0 = (v8u16)msa_extq_s16((v8i16)g0, (v8i16)g0, 1);
+            v8u16 abd0 = msa_abdq_u16(g0, nextg0);
+            zip0 = msa_rshrq_n_u16(msa_addq_u16(g0, nextg0), 1);
+
+            v8u16 g1 = msa_shrq_n_u16(msa_andq_u16(r0, maskhi), 8);
+            v8u16 g2 = msa_shrq_n_u16(msa_andq_u16(r2, maskhi), 8);
+            v8u16 abd1 = msa_abdq_u16(g2, g1);
+            zip1 = msa_rshrq_n_u16(msa_addq_u16(g2, g1), 1);
+
+            v8u16 v_cmp = msa_cleq_u16(abd0, abd1);
+            zip0 = msa_andq_u16(v_cmp, zip0);
+            v_cmp = msa_cgtq_u16(abd0, abd1);
+            zip1 = msa_andq_u16(v_cmp, zip1);
+            // g0 g1 g2 ...
+            zip1 = msa_orrq_u16(zip0, zip1);
+            zip0 = nextg0;
+            ILVRL_H2_UH(zip0, zip1, g0, g1);
+            pix[1] = msa_pack_u16(g0, g1);
+
+            r1 = msa_addq_u16(msa_andq_u16(r0, masklo), msa_andq_u16(r2, masklo));
+            v8u16 nextr1 = (v8u16)msa_extq_s16((v8i16)r1, (v8i16)r1, 1);
+            r0 = msa_addq_u16(r1, nextr1);
+            // r0 r1 r2 ...
+            zip0 = msa_rshrq_n_u16(nextr1, 1);
+            zip1 = msa_rshrq_n_u16(r0, 2);
+            ILVRL_H2_UH(zip0, zip1, r0, r1);
+            pix[blue << 1] = msa_pack_u16(r0, r1);
+
+            msa_st3q_u8(dst, pix[0], pix[1], pix[2]);
+        }
+
+        return int(bayer - (bayer_end - width));
+    }
+};
 #else
 typedef SIMDBayerStubInterpolator_<uchar> SIMDBayerInterpolator_8u;
 #endif
@@ -1049,6 +1276,59 @@ static void Bayer2RGB_VNG_8u( const Mat& srcmat, Mat& dstmat, int code )
                     _mm_storeu_si128((__m128i*)(brow + N6), b6);
                 }
             }
+#elif CV_MSA
+            for( ; i <= N - 9; i += 8, srow += 8, brow += 8 )
+            {
+                v8u16 s1, s2, s3, s4, s6, s7, s8, s9;
+                if( i <= N - 17 )
+                {
+                    v16u8 s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow - bstep - 1), msa_dupq_n_s8(0));
+                    s1 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow - bstep), msa_dupq_n_s8(0));
+                    s2 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow - bstep + 1), msa_dupq_n_s8(0));
+                    s3 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow - 1), msa_dupq_n_s8(0));
+                    s4 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow + 1), msa_dupq_n_s8(0));
+                    s6 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow + bstep - 1), msa_dupq_n_s8(0));
+                    s7 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow + bstep), msa_dupq_n_s8(0));
+                    s8 = msa_paddlq_u8(s);
+                    s = (v16u8)msa_ilvrq_s8((v16i8)msa_ld1q_u8(srow + bstep + 1), msa_dupq_n_s8(0));
+                    s9 = msa_paddlq_u8(s);
+                }
+                else
+                {
+                    s1 = msa_movl_u8(msa_ld1_u8(srow - bstep - 1));
+                    s2 = msa_movl_u8(msa_ld1_u8(srow - bstep));
+                    s3 = msa_movl_u8(msa_ld1_u8(srow - bstep + 1));
+                    s4 = msa_movl_u8(msa_ld1_u8(srow - 1));
+                    s6 = msa_movl_u8(msa_ld1_u8(srow + 1));
+                    s7 = msa_movl_u8(msa_ld1_u8(srow + bstep - 1));
+                    s8 = msa_movl_u8(msa_ld1_u8(srow + bstep));
+                    s9 = msa_movl_u8(msa_ld1_u8(srow + bstep + 1));
+                }
+
+                v8u16 b = msa_addq_u16(msa_addq_u16(msa_abdq_u16(s1, s7), msa_abdq_u16(s3, s9)),
+                                       msa_shlq_n_u16(msa_abdq_u16(s2, s8), 1));
+                msa_st1q_u16(brow, b);
+                b = msa_addq_u16(msa_addq_u16(msa_abdq_u16(s1, s3), msa_abdq_u16(s7, s9)),
+                                 msa_shlq_n_u16(msa_abdq_u16(s4, s6), 1));
+                msa_st1q_u16(brow + N, b);
+                v8u16 b2 = msa_shlq_n_u16(msa_abdq_u16(s3, s7), 1);
+                msa_st1q_u16(brow + N2, b2);
+                v8u16 b3 = msa_shlq_n_u16(msa_abdq_u16(s1, s9), 1);
+                msa_st1q_u16(brow + N3, b3);
+
+                b = msa_addq_u16(b2, msa_addq_u16(msa_abdq_u16(s2, s4), msa_abdq_u16(s8, s6)));
+                msa_st1q_u16(brow + N4, b);
+                b = msa_addq_u16(b3, msa_addq_u16(msa_abdq_u16(s2, s6), msa_abdq_u16(s8, s4)));
+                msa_st1q_u16(brow + N5, b);
+                b = msa_shrq_n_u16(msa_addq_u16(msa_addq_u16(s2, s4), msa_addq_u16(s6, s8)), 1);
+                msa_st1q_u16(brow + N6, b);
+            }
 #endif
 
             for( ; i < N-1; i++, srow++, brow++ )
@@ -1153,14 +1433,14 @@ static void Bayer2RGB_VNG_8u( const Mat& srcmat, Mat& dstmat, int code )
                     {
                         Rs += srow[-bstep*2-2] + srow[0];
                         Gs += brow0[N6-1];
-                        Bs += srow[-bstep+1]*2;
+                        Bs += srow[-bstep-1]*2;
                         ng++;
                     }
                     if( gradSE < T )
                     {
                         Rs += srow[bstep*2+2] + srow[0];
                         Gs += brow2[N6+1];
-                        Bs += srow[-bstep+1]*2;
+                        Bs += srow[bstep+1]*2;
                         ng++;
                     }
                     R = srow[0];
@@ -1394,8 +1674,8 @@ static void Bayer2RGB_VNG_8u( const Mat& srcmat, Mat& dstmat, int code )
                 RGs = _mm_adds_epi16(RGs, _mm_and_si128(_mm_merge_epi16(t1, t0), mask));
                 // GRs += {brow2[N6+1]; (srow[1]+srow[bstep*2+1])} * (T>gradSE)
                 GRs = _mm_adds_epi16(GRs, _mm_and_si128(_mm_merge_epi16(_mm_loadu_si128((__m128i*)(brow2+N6+1)), _mm_adds_epi16(x7,x10)), mask));
-                // Bs  += {srow[-bstep+1]*2; (srow[bstep+2]+srow[bstep])} * (T>gradSE)
-                Bs  = _mm_adds_epi16(Bs, _mm_and_si128(_mm_merge_epi16(_mm_slli_epi16(x5, 1), _mm_adds_epi16(x8,x11)), mask));
+                // Bs  += {srow[bstep+1]*2; (srow[bstep+2]+srow[bstep])} * (T>gradSE)
+                Bs  = _mm_adds_epi16(Bs, _mm_and_si128(_mm_merge_epi16(_mm_slli_epi16(x9, 1), _mm_adds_epi16(x8,x11)), mask));
 
                 // gradS ***********************************************
                 mask = _mm_cmpgt_epi16(T, gradS);  // mask = T>gradS
@@ -1451,7 +1731,7 @@ static void Bayer2RGB_VNG_8u( const Mat& srcmat, Mat& dstmat, int code )
                 // GRs += {brow0[N6-1]; (srow[-bstep*2-1]+srow[-1])} * (T>gradNW)
                 GRs = _mm_adds_epi16(GRs, _mm_and_si128(_mm_merge_epi16(_mm_loadu_si128((__m128i*)(brow0+N6-1)), _mm_adds_epi16(x2,x15)), mask));
                 // Bs  += {srow[-bstep-1]*2; (srow[-bstep]+srow[-bstep-2])} * (T>gradNW)
-                Bs  = _mm_adds_epi16(Bs, _mm_and_si128(_mm_merge_epi16(_mm_slli_epi16(x5, 1),_mm_adds_epi16(x3,x16)), mask));
+                Bs  = _mm_adds_epi16(Bs, _mm_and_si128(_mm_merge_epi16(_mm_slli_epi16(x1, 1),_mm_adds_epi16(x3,x16)), mask));
 
                 __m128 ngf0 = _mm_div_ps(_0_5, _mm_cvtloepi16_ps(ng));
                 __m128 ngf1 = _mm_div_ps(_0_5, _mm_cvthiepi16_ps(ng));
